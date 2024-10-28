39d728fa8bfb12bd9aa5b8bca8f813b7d3c405ca
janine pamintuan <jaja.ninnin@gmail.com>
2024-10-28 15:03:49 -0400

    jsx lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { image } from "../data/data";
 
+// const H2 = "About me"
+
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>This is a paragraph tag</p>
+      <img src={image} alt="I made this"></img>
+    </div>);
 }
 
 export default About;
